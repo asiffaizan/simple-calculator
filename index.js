9fc81ace8093a0140a9d9dea7ae3386a7b4f7896
asiffaizan <asiffaizan0077@gmail.com>
2023-05-02 15:23:33 +0600

    complete

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
 let display = document.getElementById('display');
 let buttons = Array.from(document.getElementsByClassName('button'));
-console.log(buttons);
+// console.log(buttons);
+
+buttons.map( button => {
+    button.addEventListener('click', (e)=>{
+        // console.log('is clicked?');
+        // console.log(e);
+        // console.log(e.target);
+        // console.log(e.target.innerText);
+        switch(e.target.innerText){
+            case 'C':
+                display.innerText = '';
+                break;
+            case '←':
+                if(display.innerText){
+                    display.innerText = display.innerText.slice(0, -1);
+                }
+                break;
+            case '=':
+                try {
+                    display.innerText = eval(display.innerText);
+                } catch {
+                    display.innerText = 'Invalid Expression!'
+                }
+                break;
+            default:
+                display.innerText += e.target.innerText;
+        }
+    })
+})
